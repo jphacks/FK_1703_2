6557d2dc45c7910e635ba8f3303296d2043228d6
Kota-Y <mcz-0604-htt@outlook.com>
2017-10-29 02:35:26 +0900

    精度修正

--- relation_char.php
+++ relation_char.php
@@ -42,46 +42,6 @@ for($i=0;$i<$keyword_number;$i++)
 	}
 }
 
-$limit_number=3;
-
-$graph_data = array(
-  'query' => $keyword,
-  'limit' => $limit_number,
-  'indent' => TRUE,
-  'key' => 'AIzaSyAa48vTShzI501hNqTCvned8iRh3-S_S_Q'
-);
-
-$graph_url = 'https://kgsearch.googleapis.com/v1/entities:search' . '?' . http_build_query($graph_data);
-
-$ch = curl_init();
-curl_setopt($ch,CURLOPT_URL, $graph_url);
-curl_setopt($ch,CURLOPT_CUSTOMREQUEST,'GET');
-curl_setopt($ch,CURLOPT_SSL_VERIFYPEER,false);
-curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
-$graph_result_json = curl_exec($ch);
-$graph_result_data = json_decode($graph_result_json,true);
-curl_close($ch);
-
-$graph_keywords = array("楽しい");
-
-foreach((array)$graph_result_data['itemListElement'] as $graph_data) {
-  $graph_keywords[] = $graph_data['result']['name'];
-}
-
-for($i=1;$i<=$limit_number;$i++)
-{
-	if( strcasecmp($keyword,$graph_keywords[$i]) != 0 )
-	{
-		$graph_key = $graph_keywords[$i];
-		break;
-	}
-	$graph_key= "カプチーノ";
-}
-
-$send_keywords['relation_keyword'] = array("");
-
-$send_keywords['relation_keyword'][] = $graph_key;
-
 $suggest_data = array(
   'qu' => $keyword .'+',
   'hl' => 'ja',
@@ -106,19 +66,93 @@ $suggest_result_data = json_decode($suggest_result_json,true);
 
 $suggest_keywords=array("楽しい");
 
-foreach((array)$suggest_result_data['CompleteSuggestion'] as $suggest_data) {
-  //echo $suggest_data['suggestion']['@attributes']['data'] . '<br/>';
-	$suggest_keywords[] = $suggest_data['suggestion']['@attributes']['data'];
+foreach((array)$suggest_result_data['CompleteSuggestion'] as $suggest_data)
+{
+ 	$suggest_keywords[] = $suggest_data['suggestion']['@attributes']['data'];
 }
 
 for($i=1;$i<=10;$i++)
 {
-	$suggest_keyword[$i]=str_replace($keyword, "", $suggest_keywords[$i]);
+	$suggest_keyword[$i]=trim(str_replace($keyword, "", $suggest_keywords[$i]));
 }
 
-$rand_keys = array_rand($suggest_keyword, 2);
-$send_keywords['relation_keyword'][] = $suggest_keyword[$rand_keys[0]];
-$send_keywords['relation_keyword'][] = $suggest_keyword[$rand_keys[1]];
+$rand_keys = array_rand($suggest_keyword,9);
+
+$send_keywords['relation_keyword'] = array("");
+$send_number = 0;
+
+
+for($i=0;$i<9;$i++)
+{
+	if(mb_strlen($suggest_keyword[$rand_keys[$i]]) <= 15)
+	{
+		$send_keywords['relation_keyword'][] = $suggest_keyword[$rand_keys[$i]];
+		$send_number++;
+		if($send_number == 2)
+		{
+			break;
+		}
+	}
+	else
+	{
+		continue;
+	}
+}
+
+//$send_keywords['relation_keyword'][] = $suggest_keyword[$rand_keys[1]];
+
+$limit_number=3;
+
+$graph_data = array(
+  'query' => $keyword,
+  'limit' => $limit_number,
+  'indent' => TRUE,
+  'key' => 'AIzaSyAa48vTShzI501hNqTCvned8iRh3-S_S_Q'
+);
+
+$graph_url = 'https://kgsearch.googleapis.com/v1/entities:search' . '?' . http_build_query($graph_data);
+
+$ch = curl_init();
+curl_setopt($ch,CURLOPT_URL, $graph_url);
+curl_setopt($ch,CURLOPT_CUSTOMREQUEST,'GET');
+curl_setopt($ch,CURLOPT_SSL_VERIFYPEER,false);
+curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+$graph_result_json = curl_exec($ch);
+$graph_result_data = json_decode($graph_result_json,true);
+curl_close($ch);
+
+$graph_keywords = array("楽しい");
+
+
+if($graph_result_data['itemListElement'] == null)
+{
+	if(mb_strlen($suggest_keyword[$rand_keys[8]]) <= 15)
+	{
+		$send_keywords['relation_keyword'][] = $suggest_keyword[$rand_keys[8]];
+	}
+	else
+	{
+		$send_keywords['relation_keyword'][] = "カプチーノ";
+	}
+}
+else
+{
+	foreach((array)$graph_result_data['itemListElement'] as $graph_data)
+	{
+ 		$graph_keywords[] = $graph_data['result']['name'];
+	}
+
+	for($i=1;$i<=$limit_number;$i++)
+	{
+		if( strcasecmp($keyword,$graph_keywords[$i]) != 0 )
+		{
+			$graph_key = $graph_keywords[$i];
+			break;
+		}
+		$graph_key= "カプチーノ";
+	}
+	$send_keywords['relation_keyword'][] = $graph_key;
+}
 
 $relation_keyword_split = array_splice($send_keywords['relation_keyword'],0,1);
 
